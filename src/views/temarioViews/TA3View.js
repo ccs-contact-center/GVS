81009b1a61b755dfd1668c9051c0b32dab254945
DanielDO93 <danieldionisio1993@gmail.com>
2021-06-03 14:42:27 -0500

    Update TA3View.js

--- src/views/temarioViews/TA3View.js
+++ src/views/temarioViews/TA3View.js
@@ -15,7 +15,7 @@ class TA3View extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-         <CardHeader className="text-left bgH" style={{paddingBottom:"0px"}}>
+         <CardHeader className="text-left" style={{paddingBottom:"0px"}}>
           <h5> GESTION DE VANTAS, VENCIMIENTO DE OBJECIONES.</h5>
           <h5> &nbsp;&nbsp;-1.1 Que es el vencimiento o  manejo de objeciones.</h5>
         </CardHeader>
