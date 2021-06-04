5838d5c7a3537cb11507c010d9dc9f8986bb187b
DanielDO93 <danieldionisio1993@gmail.com>
2021-06-04 13:59:36 -0500

    Update TA4View.js

--- src/views/temarioViews/TA4View.js
+++ src/views/temarioViews/TA4View.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
-import { CardBody, Col, Row ,Spinner,CardHeader} from 'reactstrap'
+import { CardBody, Col, Row, Spinner, CardHeader } from 'reactstrap'
 import ta1_2 from '../../assets/img/safelite/ta1-2.png'
 
 class TA4View extends Component {
   loading = () => (
-    <div className="animated fadeIn pt-3 text-center "><Spinner  color="danger"  /></div>
+    <div className="animated fadeIn pt-3 text-center "><Spinner color="danger" /></div>
   )
   render() {
     return (
       <div className="animated fadeIn">
-         <CardHeader className="text-left " style={{paddingBottom:"0px"}}>
+        <CardHeader className="text-left " style={{ paddingBottom: "0px" }}>
           <h5> GESTION DE VANTAS, VENCIMIENTO DE OBJECIONES.</h5>
           <h5> &nbsp;&nbsp;-1.1 Que es el vencimiento o  manejo de objeciones.</h5>
         </CardHeader>
@@ -24,15 +24,11 @@ class TA4View extends Component {
             </Col>
             <Col xs="8" className="text-justify">
               <p>
-               <b>QUE ES EL VENCIMIENTO O MANEJO DE OBJECIONES</b>  
-             </p>
+                <b>QUE ES EL VENCIMIENTO O MANEJO DE OBJECIONES</b>
+              </p>
               <p>
-                El cliente presenta dudas en cuanto al precio, valor, relevancia del producto o la posibilidad 
-                económica que tiene para comprarlo.
-             </p>
-             <p>El manejo de objeciones es la habilidad de  identificar  las necesidades del cliente mediante el 
-               sondeo, de esta manera poder responder a la objeción con un beneficio objetivo y adecuado para el 
-               cliente.</p>
+                El manejo de objeciones es <b>la habilidad de  identificar  las necesidades del cliente</b> mediante el sondeo,
+              de esta manera poder responder a la objeción con un beneficio objetivo y adecuado para el cliente.</p>
             </Col>
           </Row>
         </CardBody>
